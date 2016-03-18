a57a1c97e9753408d13c7dfb412d472df6a462ee
RomeroFederico <romerof14@gmail.com>
2016-03-18 18:47:04 -0300

    Creacion del programa.

---
+++ Ejercicio_9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_9
+{
+    class Ejercicio_9
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio Nro. 9";
+        }
+    }
+}
